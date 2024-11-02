20649e4f2aebaef6ba689d21b8f6b904818334ee
sujan subedi <sujansubedinp@gmail.com>
2024-11-02 16:10:59 +0545

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Dynamically update the copyright year
+document.addEventListener("DOMContentLoaded", () => {
+    const yearElement = document.getElementById("year");
+    yearElement.textContent = new Date().getFullYear();
+  });
+  
